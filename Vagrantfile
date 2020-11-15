e5169f0d9b886766a3701adb62ed794b4ce95cbd
jamg44 <jamg44@gmail.com>
2020-11-15 15:35:04 +0100

    usar variables para las passwords

--- Vagrantfile
+++ Vagrantfile
@@ -7,12 +7,11 @@ Vagrant.configure("2") do |config|
     ubuntu1.vm.hostname = "ubuntu1"
     ubuntu1.vm.network "private_network", ip: "192.168.100.10", nic_type: "virtio", virtualbox__intnet: "keepcoding"
     ubuntu1.vm.network "forwarded_port", guest: 80, host: 8081
-    ubuntu1.vm.network "forwarded_port", guest: 9090, host: 9091
     ubuntu1.vm.provider "virtualbox" do |vb|
       vb.memory = "512"
       vb.default_nic_type = "virtio"
     end
-    ubuntu1.vm.provision :shell, :path => "install.sh"
+    ubuntu1.vm.provision :shell, :path => "install_wordpress.sh"
     # ubuntu1.vm.synced_folder ".", "/var/www"
   end
   # config.vm.define "ubuntu2" do |ubuntu2|
@@ -21,7 +20,6 @@ Vagrant.configure("2") do |config|
   #   ubuntu2.vm.hostname = "ubuntu2"
   #   ubuntu2.vm.network "private_network", ip: "192.168.100.11", nic_type: "virtio", virtualbox__intnet: "keepcoding"
   #   ubuntu2.vm.network "forwarded_port", guest: 80, host: 8082
-  #   ubuntu2.vm.network "forwarded_port", guest: 9090, host: 9092
   #   ubuntu2.vm.provider "virtualbox" do |vb|
   #     vb.memory = "512"
   #     vb.default_nic_type = "virtio"
--- install_wordpress.sh
+++ install_wordpress.sh
@@ -1,9 +1,12 @@
 
 # Install Wordpress (https://ubuntu.com/tutorials/install-and-configure-wordpress)
 
+MYSQL_ROOT_PASSWORD="rootpwd"
+MYSQL_WORDPRESS_PASSWORD="wpress"
+
 sudo apt-get update
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 sudo apt install -y wordpress php libapache2-mod-php mysql-server php-mysql
 
 # Crear el site de wordpress
@@ -28,7 +31,7 @@ cat << EOF | sudo tee /var/www/html/index.html
 <!DOCTYPE html>
 <html lang="en">
   <head><meta charset="utf-8"></head>
-  <body><h1>Para usar wordpress vaya a <a href="/blog">/blog</a></h1></body>
+  <body><h1>Wordpress desplegado en <a href="/blog">/blog</a></h1></body>
 </html>
 EOF
 
@@ -37,15 +40,15 @@ sudo a2enmod rewrite
 sudo service apache2 restart
 
 # Crear la base de datos
-mysql -u root -proot -e "CREATE DATABASE wordpress;"
-mysql -u root -proot -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost IDENTIFIED BY 'wordpress';"
-mysql -u root -proot -e "FLUSH PRIVILEGES;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE wordpress;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost IDENTIFIED BY '$MYSQL_WORDPRESS_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
 cat << EOF | sudo tee /etc/wordpress/config-localhost.php
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', '$MYSQL_WORDPRESS_PASSWORD');
 define('DB_HOST', 'localhost');
 define('DB_COLLATE', 'utf8_general_ci');
 define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
